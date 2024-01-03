d81c684a2b126e74239bf9dbae30e69b0da74cbc
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2024-01-03 15:15:13 -0700

    Validate args

--- args.sh
+++ args.sh
@@ -6,6 +6,24 @@ setOutputVar() {
   echo "${1}=$2" | tee -a "$GITHUB_OUTPUT" &> /dev/null
 }
 
+# Validate GITHUB_* variables that are used
+if [ -z "$GITHUB_WORKSPACE" ]; then
+  echo "GITHUB_WORKSPACE is missing"
+  exit 1
+fi
+if ! [ -d "$GITHUB_WORKSPACE" ]; then
+  echo "${GITHUB_WORKSPACE} does not exist"
+  exit 1
+fi
+if ! [ -f "$GITHUB_OUTPUT" ]; then
+  echo "${GITHUB_OUTPUT} does not exist"
+  exit 1
+fi
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "GITHUB_OUTPUT is missing"
+  exit 1
+fi
+
 if [ -z "$ccache_key" ]; then
   setOutputVar ccache_key "$default_ccache_key"
 else
@@ -14,13 +32,27 @@ fi
 
 setOutputVar extra_flags "$(echo "$extra_flags" | toArray)"
 
-src_path="$(realpath "$src_path")"
-main="$(realpath "$main")"
-main_path="${src_path}/${main}"
+if ! [ -d "$src_path" ]; then
+  echo "${src_path} does not exist"
+  exit 1
+fi
+src_path="$(realpath --canonicalize-missing "$src_path")"
+cd "$src_path"
+if [ -f "$main" ] || [ -d "$main" ]; then
+  main="$(
+    realpath \
+      --relative-to="$src_path" \
+      "$main"
+  )"
+else
+  echo "${main} does not exist"
+  exit 1
+fi
+main="$(realpath "${src_path}/${main}")"
 
 setOutputVar main_file "$(
   realpath \
     --canonicalize-missing \
     --relative-to="$GITHUB_WORKSPACE" \
-    "${src_path}/${main}"
+    "$main"
 )"
